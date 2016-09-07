c1bdbb68767dda4d7cd672d304ab1fbc77717318
Fasic <wasicfilip93@gmail.com>
2016-09-07 16:20:41 +0200

    v1.3

--- app/src/main/java/com/fasic/fasic/muzej/oAktivnost.java
+++ app/src/main/java/com/fasic/fasic/muzej/oAktivnost.java
@@ -6,50 +6,26 @@ import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 
-public class info extends Activity {
+public class oAktivnost extends Activity {
     protected String jezik;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
-        jezik = intent.getStringExtra("jezik");
+        jezik = intent.getStringExtra("jezik");//get i o cemu je...
         setJezik(jezik);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_o_aktivnost);
 
-        setFont();
-
-
-        ImageView logo = (ImageView) findViewById(R.id.logoID);
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-
-        LinearLayout dugme1 = (LinearLayout) findViewById(R.id.scrollViewID);
-        dugme1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(info.this, oAktivnost.class);
-                intent.putExtra("jezik", jezik);
-                startActivityForResult(intent, 0);
-            }
-        });
+        setOmuzeju();
     }
 
     protected void setJezik(String jezik){
@@ -72,16 +48,35 @@ public class info extends Activity {
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
     }
 
-    private void setFont(){
+    protected void setOmuzeju(){
+        /*RelativeLayout mainView = (RelativeLayout) findViewById(R.id.mainViewID);
+        TextView header = (TextView) findViewById(R.id.headerID);
+
+        RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView opis1 = new TextView(this);
+        opis1.setText("Ovo ide iz stringa zbog jezika!");
+        mainView.addView(opis1);
+
+        ImageView slika1 = new ImageView(this);
+        slika1.setImageDrawable(getResources().getDrawable(R.drawable.muzej));
+        relativeParams.addRule(RelativeLayout.BELOW, opis1.getId());
+        mainView.addView(slika1, relativeParams);
+
+        header.setText("O muzeju");*/
+
+        setContentView(R.layout.omuzeju);
+        TextView header = (TextView) findViewById(R.id.headerID);
+        header.setText("O muzeju");
+
         Typeface font = Typeface.createFromAsset(getAssets(),  getResources().getString(R.string.font));
         font.isBold();
 
-        TextView head = (TextView) findViewById(R.id.headerID);
-        TextView muzej = (TextView) findViewById(R.id.oMuzejuID);
-        TextView risovaca = (TextView) findViewById(R.id.oRisovaci);
+        TextView opis1 = (TextView) findViewById(R.id.opis1OmuzejuID);
+        opis1.setTypeface(font);
 
-        head.setTypeface(font);
-        muzej.setTypeface(font);
-        risovaca.setTypeface(font);
+        TextView opis2 = (TextView) findViewById(R.id.opis1OmuzejuID);
+        opis2.setTypeface(font);
+        header.setTypeface(font);
     }
 }
